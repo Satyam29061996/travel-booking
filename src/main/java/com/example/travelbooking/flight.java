92f4d8e62cc555659f8302b78a16e542bccddf53
Sam Smith <sam@example.com>
2022-10-15 17:59:17 +0530

    coderefactor

---
+++ src/main/java/com/example/travelbooking/flight.java
@@ -0,0 +1,14 @@
+package com.example.travelbooking;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class flight {
+    @GetMapping("/myflight")
+    public String getData(){
+        return "Please book your hhhh Flight ticket, Choose Your Fav Airlines Idigo";
+    }
+
+}
